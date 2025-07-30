28885f59cad8e4aaa29d3f2bebb20e7334b6c5e3
djumanov <djumanovdev@gmail.com>
2025-07-30 23:07:08 +0500

    init

--- bot.py
+++ bot.py
@@ -1,19 +1,31 @@
-import config
+from config.token import TOKEN
+from config.states import SELECT_LANGUAGE, CONTACT, PHONE, CITY
 
 # import telegram modules
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler, Filters
+from telegram.ext import CallbackQueryHandler, ConversationHandler
 from telegram.ext import Updater
 
 # import handlers
 from handlers.start_handler import start
+from handlers.register import handle_language_selection, handle_contact
 
 
 def main() -> None:
-    updater = Updater(token=config.TOKEN)
+    updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
 
-    # add handlers
-    dispatcher.add_handler(CommandHandler('start', start))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            SELECT_LANGUAGE: [CallbackQueryHandler(handle_language_selection, pattern='^(uz|ru)$')],
+            CONTACT: [MessageHandler(Filters.contact, handle_contact)],
+            PHONE: [MessageHandler(Filters.contact, start)],
+            CITY: [MessageHandler(Filters.text & ~Filters.command, start)],
+        },
+        fallbacks=[],
+    )
+    dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
     updater.idle()
